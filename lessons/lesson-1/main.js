221a4a7dec6958c259cbfd3f9a79f6d275b21ca1
Ilya Aralin <aralinilya@yandex-team.ru>
2021-01-28 20:54:41 +0300

    add

---
+++ lessons/lesson-1/main.js
@@ -0,0 +1,91 @@
+/*
+const - константы
+let - переменная в областе видимости внутри {}
+var - старая хрень
+
+let iterator - просто перменные (расходный материал)
+
+let Iterator - классы
+let BottonGroup - классы
+
+const ITERATOR - важные костанты, входные данные..
+
+let bottonGroup — объединение слов (способы)
+let botton_group
+
+let $body - элементы DOM 
+*/
+
+/*
+render - отображение
+     |
+     -- css = stylsheet
+     -- DOM = html
+*/
+
+/*
+const myFunction = function(){
+    console.log('Hello world!!!');
+}
+*/
+
+/*
+function myFunction(){
+    console.log('Hello world!!!');
+}
+*/
+
+/*
+const myFunction = () => console.log('Hello world!!!');
+*/
+
+/*
+const myFunction = new Function('console.log("Hello world!!!")');
+*/
+
+/*
+//примитивы (коробочки) ( Приводимые типы данных )
+const number = 121212;
+const string = 'строка'; // '12'
+const bool = true;
+let undef = undefined; 
+
+const fn = () => console.log('function'); 
+
+//объекты (полка)
+const arr = [1,2,3,'строка', number]; //массив
+
+const arr1 = {
+    0 : 1,
+    1 : 2,
+    3 : 3,
+    4 : 'строка'
+}
+
+const obj = {
+    name : 'Маша',
+    age : [arr,31,16]
+};
+
+let nul = null; // === let nul;
+
+//obj['name'] === obj.name - обращение к ключу объекта
+
+
+let a = '1';
+let b = 1;
+let c = true // true == 1, false == 0
+let d = ' ';
+let f = {}; 
+let e = {}; 
+
+console.log( b );
+*/
+
+
+
+
+
+
+
+
