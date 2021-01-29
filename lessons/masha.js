d4f826c416b35de8ce7a037a547223f4c7e837d5
Maria B <vereskovaia@gmail.com>
2021-01-29 18:54:35 +0100

    Update masha.js

--- lessons/masha.js
+++ lessons/masha.js
@@ -34,3 +34,40 @@ const needWater = new Function ('a','b'), {
 needWater (document.getElementById("glueWeightId").value, document.getElementById("concentrationId").value);
 
 console.log (needWater)
+
+// Objects:
+
+//вредители
+const VARMENT= {
+    type: ['insect', 'gnawer']
+    };
+    
+    const insects = {
+    subspecies: ['silverfish', 'paperfish', 'anobiidae'],
+    struggle: ['nitrogen fumigation', 'freezing', 'heat', 'pesticides', 'poison']
+    };
+    
+    const gnawers = {
+    subspecies: ['mouse', 'rat'],
+    struggle: ['nitrogen fumigation', 'freezing', 'heat', 'pesticides', 'poison']
+    };
+    
+//краски по цветам
+    const COLORS = {
+    white: [ 'lead white', 'zinc white', 'titan white', 'lithopone', 'chalk'],
+    yellow: ['cadmium yellow', 'yellow citrone', 'lead tin yellow'],
+    ochre: [ 'light ochre', 'dark ochre'],
+    red: ['ironoxide', 'cadmium red'],
+    purple: ['hematite', 'kaput-mortuum'],
+    violet: ['fluorite', 'cobalt violet'] ,
+    brown: ['vam dycke brown', 'umber'],
+    black: ['peach black', 'blue black']
+    }
+    
+//Краски, другая организация, согласно химическому составу
+    
+    const COLORS = {
+    lead: [ 'lead white', 'lead-tin-yellow', 'massicot'],
+    sulfur: [ 'cadmium yellow', 'gipsum', 'oripigment', 'realgar'],
+    carbonate: ['azurite', 'malachite', 'chalk']
+     }
