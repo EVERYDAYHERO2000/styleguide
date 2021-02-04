78d7d7c49e261113dd26e6de8dd1e2301a3cac79
Maria B <vereskovaia@gmail.com>
2021-02-04 22:02:53 +0100

    Update masha.js

--- lessons/masha.js
+++ lessons/masha.js
@@ -1,4 +1,4 @@
-//first opportunity
+/* //first opportunity
 const needWater = function () {
     let weightOfWater;
 
@@ -30,6 +30,7 @@ const needWater = () => {
 /* а как здесь? это вот вообще не понятно. Есть два аргумента, но с ними, получается, нельзя больше,
 чем одну математическую операцию проводить? */
 
+/*
 const needWater = new Function (a,b) {
     return  (+a*100) / +b - +a ;
 };
@@ -106,3 +107,58 @@ function Mixcolors() {
 } };
 
 Mixcolors();
+
+*/
+
+// DE Variante
+// Конструктор
+function Farbe (name, blei, schwefel, karbonat, stabil, modern, giftig) {
+    this.name = name;
+    this.blei = blei;
+    this.schwefel = schwefel;
+    this.karbonat = karbonat;
+    this.stabil = stabil;
+    this.modern = modern;
+    this.giftig = giftig;
+    //organisch hinzufügen? Siehe ab Gelb
+
+    return this;
+}
+
+// Массив
+const farben = [
+    new Farbe ('Kreide', false, false, true, true, false, false),
+    new Farbe ('Gips', false, true, false, false, false, false),
+    new Farbe ('Bleiweiß', true, false, true, false, false, true),
+    new Farbe ('Lithopone', false, true, false, false, true, false),
+    new Farbe ('Zinkweiß', false, false, false, true, true, false),
+    new Farbe ('Chinesischweiß', false, false, false, true, true, false),
+    new Farbe ('Titanweiß', false, false, false, true, true, false),
+
+    new Farbe ('Auripigment', false, true, false, false, false, true),
+    new Farbe ('Massicot', true, false, false, false, false, true),
+    new Farbe ('Bleizinngelb', true, false, false, false, false, true),
+    new Farbe ('Gelber Ocker', false, false, false, true, false, false),
+    new Farbe ('Sienna', false, false,false, true, false, false),
+    new Farbe ('Marsgelb', false, false, false, true, true, false),
+    new Farbe ('Kadmiumgelb', false, true, false, false, true, false),
+    new Farbe ('Chromgelb', true, true, false, false, true, true),
+    new Farbe ('Zinkgelb', false, false, false, false, true, false),
+    new Farbe ('Strontiumgelb', false, false, false, false, true, false),
+    new Farbe ('Quercitron', false, false, false, false, true, false),
+    new Farbe ('Gelbkraut', false, false, false, false, false, false),
+    new Farbe ('Gelbbeeren', false, false, false, false, false, false),
+    new Farbe ('Echter Saffran', false, false, false, false, false, false),
+    new Farbe ('Gummigut', false, false, false, false, true, false),
+
+    new Farbe ('Zinnober', false, true, false, false, false, true),
+    new Farbe ('Mennige', true, false, false, false, false, true),
+    new Farbe ('Roter Ocker', false, false, false, true, false, false),
+    new Farbe ('Caput Mortuum',  false, false, false, true, false,  false),
+    new Farbe ('Realgar', false, true, false, false, false, true),
+    new Farbe ('Kadmiumrot', false, false, false, false, true, false),
+    new Farbe ('Chromrot', true, false, false, false, true, false),
+    new Farbe ('Rotholz', false, false, false, false, false, false),
+    new Farbe ('Kermes', false, false, false, false, false, false)
+
+]
